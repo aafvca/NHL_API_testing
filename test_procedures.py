f25d085c2c6df82f7b7b0ada1bef0785097cf2a8
Andres <aafvca@gmail.com>
2018-08-27 19:19:45 +0000

    Update test_procedures.py

--- test_procedures.py
+++ test_procedures.py
@@ -8,6 +8,11 @@ api_teams_roster_id = jmespath.compile(config['TEST1']['API_TEAMS_ROSTER_ID'])
 
 # For TEST1
 # Get the API response for both rosters 2016-2017 and 2017-2018
+# Input:
+#       url_team_: The url for the team in the season 2016-2017 and 2017-2018
+# Output:
+#       roster_data_1617: Roster information for season 2016-2017 and 2017-2018
+#       response_state_1617: Flah to indicate the function response_state was executed
 def roster_data_1617(url_team_1617):
     roster_data_1617, response_state_1617 = tf.get_response(url_team_1617)
     return roster_data_1617, response_state_1617
@@ -16,7 +21,11 @@ def roster_data_1718(url_team_1718):
     roster_data_1718, response_state_1718 = tf.get_response(url_team_1718)
     return roster_data_1718, response_state_1718
 
-# Creates a list based in roster id
+# Creates a list based in roster id for seasons 2016-2017 and 2017-2018
+# Input:
+#       roster_data_: Roster information for season 2016-2017 and 2017-2018
+# Output:
+#       roster_: Python list with the players id for the season specified
 def roster_1617(roster_data_1617):
     roster_1617 = api_teams_roster_id.search(roster_data_1617)
     return roster_1617
@@ -25,13 +34,25 @@ def roster_1718(roster_data_1718):
     roster_1718 = api_teams_roster_id.search(roster_data_1718)
     return roster_1718
 
-#Finding players that are part of the team in both seasons
+#Finds players that are part of the team in both seasons
+# Input:
+#       roster_: Python list with the player ID's for both seasons
+# Output:
+#       players_in_both: A python list with the ID's that are in both lists
+#       found_players: A flag to acknowledge that the function was executed
 def players_in_both(roster_1617, roster_1718):
     players_in_both, found_players = tf.find_players(roster_1617, roster_1718)
     return players_in_both, found_players
 
 # For TEST2
-# Reusing players_in_both to collect the points information from people function
+# Reusing players_in_both from TEST1 to collect the points information from people function
+# Input:
+#       url_people, players_in_both and url_stats: Will create the full url leading to the players stats
+#       api_points: The jmespath filter to collect the information needed in this case the players points
+# Output:
+#       roster_*_points: A python list with the players points
+#       collect_1617, response_state_1617: Flags to acknowledge that the function was executed
+#       empty_value_1617: An indicator for empty values in the lists
 def roster_1617_points(url_people,players_in_both,url_stats_1617,api_points):
     roster_1617_points, empty_value_1617, collect_1617, response_state_1617 = tf.create_list_multiple(url_people,players_in_both,url_stats_1617,api_points)
     return roster_1617_points, empty_value_1617, collect_1617, response_state_1617
@@ -40,7 +61,12 @@ def roster_1718_points(url_people,players_in_both,url_stats_1718,api_points):
     roster_1718_points, empty_value_1718, collect_1718, response_state_1718 = tf.create_list_multiple(url_people,players_in_both,url_stats_1718,api_points)
     return roster_1718_points, empty_value_1718, collect_1718, response_state_1718
 
-# Calculate team points
+# Calculate team points based in the roster_points
+# Input: 
+#       roster_*_points: A python list with the individual points per season
+# Output:
+#       team_points_: A total of the players points in a season
+#       calculate_*: Flag to acknowledge that the function was executed
 def team_points_1617(roster_1617_points):
     team_points_1617, calculate_1617 = tf.calculate_team_points(roster_1617_points)
     return team_points_1617, calculate_1617
@@ -48,20 +74,31 @@ def team_points_1617(roster_1617_points):
 def team_points_1718(roster_1718_points):
     team_points_1718, calculate_1718 = tf.calculate_team_points(roster_1718_points)
     return team_points_1718, calculate_1718
+    
 # For TEST3
 # Using roster_1718 to find if there is a difference between teams and people functions
 # Getting the teams using teams functions
 
 # Creates a list based in the team
+# Input:
+#       url_people,roster_1718,key: Will create the url to the players stats
+#       api_people_currentTeam: The jmespath filter to collect the information needed in this case the players currentTeam
+# Output:
+#       people_current_team: A python list with the team per player
+#       t3_collect_current, t3_response_state_current: Flags to acknowledge that the function was executed
+#       t3_empty_value_current: An indicator for empty values in the lists
 def people_current_team(url_people,roster_1718,key,api_people_currentTeam):
     people_current_team, t3_empty_value_current, t3_collect_current, t3_response_state_current  = tf.create_list_multiple(url_people,roster_1718,key,api_people_currentTeam)
     return people_current_team, t3_empty_value_current, t3_collect_current, t3_response_state_current
 
-# Create a list with the player position using the teams function
-#def teams_position(roster_data_1718):
-#    teams_position = api_teams_position.search(roster_data_1718)
-
 # Create a list with the player position using the people function
+# Input:
+#       url_people,roster_1718,key: Will create the url to the players stats
+#       api_people_position: The jmespath filter to collect the information needed in this case the players position
+# Output:
+#       people_position: A python list with the position per player
+#       t3_collect_position, t3_response_state_position: Flags to acknowledge that the function was executed
+#       t3_empty_value_position: An indicator for empty values in the lists
 def people_position(url_people,roster_1718,key,api_people_position):
     people_position, t3_empty_value_position, t3_collect_position, t3_response_state_position = tf.create_list_multiple(url_people,roster_1718,key,api_people_position)
     return people_position, t3_empty_value_position, t3_collect_position, t3_response_state_position
