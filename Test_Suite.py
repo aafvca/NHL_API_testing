e7b83cd592bf7e1d01d1ba9c4b3f4e59955bf4c4
Andres Flores <aafvca@gmail.com>
2018-09-01 15:33:42 -0400

    Fix missing t_noexec in Suite and functions

--- Test_Suite.py
+++ Test_Suite.py
@@ -4,6 +4,5 @@ import Test1, Test2, Test3
 # Test summary using the # of test cases pass, fail and not executed from Test1 Test2 and Test3
 t_pass = Test1.t_pass + Test2.t_pass + Test3.t_pass
 t_fail = Test1.t_fail + Test2.t_fail + Test3.t_fail
-t_noexec = Test1.t_noexec + Test2.t_noexec + Test3.t_noexec
 
-tf.test_summary(t_pass,t_fail,t_noexec)
+tf.test_summary(t_pass,t_fail)
--- test_functions.py
+++ test_functions.py
@@ -133,9 +133,9 @@ def test_fail(tc_label, fail_string):
 #       t_pass: The number of TC's passed
 #       t_fail: The number of TC's failed
 #       t_noexec: The number of TC's not executed
-def test_summary(t_pass,t_fail,t_noexec):
+def test_summary(t_pass,t_fail):
     print(ts_str)
-    print('PASS: ' + str(t_pass) + "\n" + "FAIL: " + str(t_fail) + "\n" + "NOT EXECUTED: " + str(t_noexec))
+    print('PASS: ' + str(t_pass) + "\n" + "FAIL: " + str(t_fail))
     print(separator)
 
 # Function to create logfiles for verification, needs a python list of variables to collect
